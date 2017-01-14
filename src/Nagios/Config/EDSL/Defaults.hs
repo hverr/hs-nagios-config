ae0865ad177f483a688bf62e01d5e8afa192f32b
Henri Verroken <henriverroken@gmail.com>
2017-01-14 16:48:04 +0100

    Bundle defaults in module

---
+++ src/Nagios/Config/EDSL/Defaults.hs
@@ -0,0 +1,9 @@
+module Nagios.Config.EDSL.Defaults (
+  module X
+) where
+
+import Nagios.Config.EDSL.Defaults.Commands as X
+import Nagios.Config.EDSL.Defaults.Contacts as X
+import Nagios.Config.EDSL.Defaults.Hosts as X
+import Nagios.Config.EDSL.Defaults.Services as X
+import Nagios.Config.EDSL.Defaults.TimePeriods as X
